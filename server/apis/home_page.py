4c9e458638bf8155985c36462f9af2e7051f3180
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-06-14 23:38:12 -0500

    ready for deployment

--- server/apis/home_page.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from config import api
-
-class HomePage(Resource):
-    def get(self):
-        return {'message': '200: Welcome to our Home Page'}, 200
-
-api.add_resource(HomePage, '/', endpoint='home-page')
